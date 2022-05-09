b6a163e927bd9f487db22655b21ccbce4a9febdf
arscool3 <45947743+arscool3@users.noreply.github.com>
2022-05-09 18:35:44 +0600

    added abstractions

---
+++ bot/bot.py
@@ -0,0 +1,6 @@
+class Bot:
+    def __init__(self, number_of_users: int, max_posts: int, max_likes: int):
+        self.number_of_users = number_of_users
+        self.max_posts = max_posts
+        self.max_likes = max_likes
+    
